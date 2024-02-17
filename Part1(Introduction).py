b8d30fdb9ed5ca8d5a5db235c73d34b2717b3fda
sivanantham1998 <curlysiva@gmail.com>
2024-02-17 11:41:19 +0530

    Python demo

---
+++ Part1(Introduction).py
@@ -0,0 +1,38 @@
+##Python is programming language.
+## What is programming language? Programming language that are easily communicate with the computer.here are so many of programming language like c,c++,java,etc..
+## Why python ? Python is easy to learn and its most usefull for machine learning , Data Analystics, Data science like future trends
+
+print('Hello world')
+
+## variables and variable name
+
+a=10;
+print(a)
+
+## here a is called variable name ,varables are used to define a values with particular name
+
+##type of variable
+##two types if a varable contain numbers thats called interger , if it contain thats called string
+
+b="sivanantham"
+print(type(b))
+c=10;
+print(type(c))
+
+x=int("10")
+y=int(10);
+z=x+y;
+print(z)
+
+##input from user
+name=input("enter ur name:")
+print(name)
+
+##type casting
+
+f=int(input('enter a vlaue:'))
+print(f/2)
+print(type(f/2))
+
+
+
---
+++ Part1Task.py
@@ -0,0 +1,18 @@
+## 1.Get input for variable for name and age print it.
+
+name=input("Enter name:")
+age=input("Enter age:")
+print('My name is:',name)
+print('My age is:',age)
+
+## 2.Get Integer for input variables a,b,c multiply all 3 variables , add all 3 variables Divide the Multiplied values by added value print it.
+
+a=int(input())
+b=int(input())
+c=int(input())
+multiply=int(a*b*c)
+add=int(a+b+c)
+final=multiply/add;
+print(multiply)
+print(add)
+print(final)
